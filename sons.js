4de9565757d154e011937df1891800cec6df9ccf
Samuel Seguins <85202953+samseg01@users.noreply.github.com>
2023-11-27 15:10:41 -0300

    Update sons.js

--- sons.js
+++ sons.js
@@ -1,108 +1,96 @@
 const btnDo = document.getElementById("do");
-const sonDo = new Audio('do.mp3');
+const sonDo = new Audio('notas/do.mp3');
 const btnRe = document.getElementById("re");
-const sonRe = new Audio('re.mp3');
+const sonRe = new Audio('notas/re.mp3');
 const btnMi = document.getElementById("mi");
-const sonMi = new Audio('mi.mp3');
+const sonMi = new Audio('notas/mi.mp3');
 const btnFa = document.getElementById("fa");
-const sonFa = new Audio('fa.mp3');
+const sonFa = new Audio('notas/fa.mp3');
 const btnSol = document.getElementById("sol");
-const sonSol = new Audio('sol.mp3');
+const sonSol = new Audio('notas/sol.mp3');
 const btnLa = document.getElementById("la");
-const sonLa = new Audio('la.mp3');
+const sonLa = new Audio('notas/la.mp3');
 const btnSi = document.getElementById("si");
-const sonSi = new Audio('si.mp3');
+const sonSi = new Audio('notas/si.mp3');
 
 const btnDoSus = document.getElementById("do-");
-const sonDoSus = new Audio('do-.mp3');
+const sonDoSus = new Audio('notas/do-.mp3');
 const btnReSus = document.getElementById("re-");
-const sonReSus = new Audio('re-.mp3');
+const sonReSus = new Audio('notas/re-.mp3');
 const btnFaSus = document.getElementById("fa-");
-const sonFaSus = new Audio('fa-.mp3');
+const sonFaSus = new Audio('notas/fa-.mp3');
 const btnSolSus = document.getElementById("sol-");
-const sonSolSus = new Audio('sol-.mp3');
+const sonSolSus = new Audio('notas/sol-.mp3');
 const btnLaSus = document.getElementById("la-");
-const sonLaSus = new Audio('la-.mp3');
+const sonLaSus = new Audio('notas/la-.mp3');
 
 function teclaDo() {
     sonDo.currentTime = 0;
     sonDo.volume = 1;
-    sonDo.pause();
     sonDo.play();
 }
 function teclaRe() {
     sonRe.currentTime = 0;
     sonRe.volume = 1;
-    sonRe.pause();
     sonRe.play();
 }
 function teclaMi() {
     sonMi.currentTime = 0;
     sonMi.volume = 1;
-    sonMi.pause();
     sonMi.play();
 }
 function teclaFa() {
     sonFa.currentTime = 0;
     sonFa.volume = 1;
-    sonFa.pause();
     sonFa.play();
 }
 function teclaSol() {
     sonSol.currentTime = 0;
     sonSol.volume = 1;
-    sonSol.pause();
     sonSol.play();
 }
 function teclaLa() {
     sonLa.currentTime = 0;
     sonLa.volume = 1;
-    sonLa.pause();
     sonLa.play();
 }
 function teclaSi() {
     sonSi.currentTime = 0;
     sonSi.volume = 1;
-    sonSi.pause();
     sonSi.play();
 }
 
-btnDo.addEventListener('mousedown', teclaDo);
-btnRe.addEventListener('mousedown', teclaRe);
-btnMi.addEventListener('mousedown', teclaMi)
-btnFa.addEventListener('mousedown', teclaFa)
-btnSol.addEventListener('mousedown', teclaSol)
-btnLa.addEventListener('mousedown', teclaLa)
-btnSi.addEventListener('mousedown', teclaSi)
+btnDo.addEventListener('click', teclaDo);
+btnRe.addEventListener('click', teclaRe);
+btnMi.addEventListener('click', teclaMi)
+btnFa.addEventListener('click', teclaFa)
+btnSol.addEventListener('click', teclaSol)
+btnLa.addEventListener('click', teclaLa)
+btnSi.addEventListener('click', teclaSi)
 ////////////////////////////////////////////
 function teclaDoSus() {
     sonDoSus.currentTime = 0;
     sonDoSus.volume = 1;
-    sonDoSus.pause();
     sonDoSus.play();
 }
 function teclaReSus() {
     sonReSus.currentTime = 0;
     sonReSus.volume = 1;
-    sonReSus.pause();
     sonReSus.play();
 }
 function teclaFaSus() {
     sonFaSus.currentTime = 0;
     sonFaSus.volume = 1;
-    sonFaSus.pause();
     sonFaSus.play();
 }
 function teclaSolSus() {
     sonSolSus.currentTime = 0;
     sonSolSus.volume = 1;
-    sonSolSus.pause();
     sonSolSus.play();
 }
 function teclaLaSus() {
     sonLaSus.currentTime = 0;
     sonLaSus.volume = 1;
-    sonLaSus.pause();
     sonLaSus.play();
 }
 btnDoSus.addEventListener('mouseup', teclaDoSus)
@@ -114,7 +102,7 @@ btnLaSus.addEventListener('mouseup', teclaLaSus)
 document.addEventListener('keydown', function(e) {
     e = e || window.event;
     var code = e.whitch || e.keyCode;
-
+    console.log(code);
     switch(code){
         case 90:
             teclaDo();
